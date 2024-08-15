2dfac19c85c29a01d26f4026f4f5e45c9b9cf059
sullymiller2024 <sully.miller.ai@gmail.com>
2024-08-15 15:54:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ import pdfplumber
 from openai import OpenAI
 app = Flask(__name__)
 app.config.update(
-    CELERY_BROKER_URL = 'redis://localhost:6379/0',
-    CELERY_RESULT_BACKEND='redis://localhost:6379/0'
+    CELERY_BROKER_URL = os.environ.get('REDIS_URL'),
+    CELERY_RESULT_BACKEND=os.environ.get('REDIS_URL')
 )
 def make_celery(app):
     celery = Celery(
